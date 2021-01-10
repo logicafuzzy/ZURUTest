e01b0870dc02c224f74273f5c78458fa4761f4de
michele.antolini@tiscali.it <michele.antolini@tiscali.it>
2021-01-10 17:10:54 +0100

    Starting point

---
+++ Source/ZURUTest/ZURUTest.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "ZURUTest.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ZURUTest, "ZURUTest" );
