8b3f560caf3f80656a722cb3250ab31c5e93fa94
michele.antolini@tiscali.it <michele.antolini@tiscali.it>
2021-01-20 17:49:39 +0100

    Updated parametrictablegenerator

--- Plugins/ProceduralGeneration/Source/ProceduralGeneration/Public/Generators/ParametricTableGenerator.h
+++ Plugins/ProceduralGeneration/Source/ProceduralGeneration/Public/Generators/ParametricTableGenerator.h
@@ -3,14 +3,20 @@
 #include "CoreMinimal.h"
 
 #include "Generators/ParametricGenerator.h"
-
-//TODO: remove this
 #include "Generators/TableGenerator.h"
 
+//deprecated
 class PROCEDURALGENERATION_API FParametricTableGenerator : public FParametricGenerator
 {
 public:
 	FParametricTableGenerator();
 
+	FName WidthParamName;
+	FName LengthParamName;
+	FName HeightParamName;
+
 	virtual FMeshShapeGenerator& Generate() override;
+
+protected:
+	FTableGenerator* TableGenerator;
 };
