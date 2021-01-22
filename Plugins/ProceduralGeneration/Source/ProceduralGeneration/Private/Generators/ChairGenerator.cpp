f6f286d98fdc185c67af9ec89a20f54eb6f1225a
michele.antolini@tiscali.it <michele.antolini@tiscali.it>
2021-01-22 10:27:26 +0100

    Update ChairGenerator.cpp

--- Plugins/ProceduralGeneration/Source/ProceduralGeneration/Private/Generators/ChairGenerator.cpp
+++ Plugins/ProceduralGeneration/Source/ProceduralGeneration/Private/Generators/ChairGenerator.cpp
@@ -28,6 +28,8 @@ FMeshShapeGenerator& FChairGenerator::Generate()
 
 	//TOP
 	FGridBoxMeshGenerator GridBoxMeshGenerator;
+	GridBoxMeshGenerator.bScaleUVByAspectRatio = false;
+
 	//Gridbox has origin in the center and has 2x box extent size
 	GridBoxMeshGenerator.Box = FOrientedBox3d({ hW, hL, height - hTTop }, { hW, hL, hTTop });
 	MeshGeneratorUtils::MergeGeneratedMesh(GridBoxMeshGenerator.Generate(), *this);
